e9bf58b6dae1ad77efd49a91a95d3b8ed766cfee
David Fales <davidfales@Davids-Mac-mini.local>
2023-07-23 11:17:06 -0600

    added auth fix and added new dev functionality

--- src/pages/dev/index.tsx
+++ src/pages/dev/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 import Navbar from "~/components/Navbar";
 import Footer from "~/components/Footer";
@@ -25,14 +24,21 @@ type Submission = {
 };
 
 function IndexPage() {
-  const { data: session } = useSession();
+  const [password, setPassword] = useState("");
   const { data, isLoading, isError } =
     api.submission.all.useQuery<Submission[]>();
 
-  if (isLoading) return <p>Not Logged in</p>;
+  if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>An error occurred...</p>;
-  if (session?.user.id !== process.env.NEXT_PUBLIC_USER_ID)
-    return <h1 className="text-5xl font-bold">Not Validated</h1>;
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    if (password !== process.env.NEXT_PUBLIC_PASSWORD) {
+      alert("Invalid Password!");
+      return;
+    }
+  };
 
   function formatDate(dateString: string) {
     const date = new Date(dateString);
@@ -42,61 +48,81 @@ function IndexPage() {
   return (
     <div>
       <Navbar />
-      <h1 className="text-3xl font-bold">Your Submissions</h1>
-      {data.map((submission, index) => {
-        return (
-          <div key={index} className="my-4 rounded-lg bg-white p-6 shadow-md">
-            <p className="text-lg font-semibold">
-              <strong>Submission ID:</strong> {submission.id}
-            </p>
-            <p>
-              <strong>Complexity:</strong> {submission.complexity}
-            </p>
-            <p>
-              <strong>Revisions:</strong> {submission.revisions ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Pages:</strong> {submission.pages ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Database:</strong> {submission.database ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Storage:</strong> {submission.storage ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>User Base:</strong> {submission.userBase ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Monthly:</strong> {submission.monthly ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Design:</strong> {submission.design ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Database Scale:</strong>{" "}
-              {submission.databaseScale ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Storage Scale:</strong>{" "}
-              {submission.storageScale ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>User Scale:</strong> {submission.userScale ? "Yes" : "No"}
-            </p>
-            <p>
-              <strong>Cost:</strong> {submission.cost}
-            </p>
-            <p>
-              <strong>Created At:</strong> {formatDate(submission.createdAt)}
-            </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter password"
+        />
+      </form>
+      {password === process.env.NEXT_PUBLIC_PASSWORD &&
+        data.map((submission, index) => {
+          return (
+            <div>
+              <h1 className="text-3xl font-bold">Your Submissions</h1>
 
-            <p>
-              <strong>Description:</strong> {submission.description}
-            </p>
-          </div>
-        );
-      })}
+              <div
+                key={index}
+                className="my-4 rounded-lg bg-white p-6 shadow-md"
+              >
+                <p className="text-lg font-semibold">
+                  <strong>Submission ID:</strong> {submission.id}
+                </p>
+                <p>
+                  <strong>Complexity:</strong> {submission.complexity}
+                </p>
+                <p>
+                  <strong>Revisions:</strong>{" "}
+                  {submission.revisions ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Pages:</strong> {submission.pages ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Database:</strong>{" "}
+                  {submission.database ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Storage:</strong> {submission.storage ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>User Base:</strong>{" "}
+                  {submission.userBase ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Monthly:</strong> {submission.monthly ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Design:</strong> {submission.design ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Database Scale:</strong>{" "}
+                  {submission.databaseScale ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Storage Scale:</strong>{" "}
+                  {submission.storageScale ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>User Scale:</strong>{" "}
+                  {submission.userScale ? "Yes" : "No"}
+                </p>
+                <p>
+                  <strong>Cost:</strong> {submission.cost}
+                </p>
+                <p>
+                  <strong>Created At:</strong>{" "}
+                  {formatDate(submission.createdAt)}
+                </p>
+
+                <p>
+                  <strong>Description:</strong> {submission.description}
+                </p>
+              </div>
+            </div>
+          );
+        })}
 
       <Footer />
     </div>
