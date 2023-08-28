39853b933417567a3506864d3458e80e0ba74d71
David Fales <davidfalesct@gmail.com>
2023-08-28 12:46:32 -0600

    added app seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import "~/styles/globals.css";
 import Head from "next/head";
 import "aos/dist/aos.css";
 
-
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
@@ -14,9 +13,34 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Head>
-        <title>CodeCrafty</title>
+        <title>Code Crafty - Web Development & Design Services</title>
+        <meta
+          name="description"
+          content="Code Crafty offers premium web development and design services, tailoring custom websites for individuals and businesses of all sizes. Turn your vision into a digital reality with us."
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="./logo1.png" />
+
+        {/* Open Graph / Social Media */}
+        <meta
+          property="og:title"
+          content="Code Crafty - Web Development & Design Experts"
+        />
+        <meta
+          property="og:description"
+          content="Experience excellence in web design and development. Partner with Code Crafty to craft your next digital masterpiece."
+        />
+        <meta property="og:image" content="https://codecrafty.dev/banner.jpg" />
+        <meta
+          property="og:image:alt"
+          content="A representation of Code Crafty's premium services"
+        />
+        <meta property="og:url" content="https://codecrafty.dev" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* Canonical */}
+        <link rel="canonical" href="https://codecrafty.dev" />
       </Head>
       <Component {...pageProps} />
     </SessionProvider>
