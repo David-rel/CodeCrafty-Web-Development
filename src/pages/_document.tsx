4265d5d755a9651cbd3bf58b850d7bbe3c9785de
David Fales <davidfalesct@gmail.com>
2023-11-22 22:13:57 -0700

    added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
         <Head>
           {/* Other tags */}
 
-          {/* Google Analytics */}
+          {/* Existing Google Analytics Script */}
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-71QP3X0V8R"
@@ -30,6 +30,22 @@ class MyDocument extends Document {
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-71QP3X0V8R');
+              `,
+            }}
+          ></script>
+
+          {/* New Google Tag (gtag.js) Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-11420570446"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-11420570446');
               `,
             }}
           ></script>
