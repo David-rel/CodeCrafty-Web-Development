479a61b074afbca8ffabcc9350bca1db28feb194
David Fales <davidfalesct@gmail.com>
2023-11-26 13:03:28 -0700

    sitemap update:

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,26 @@
 module.exports = {
-  siteUrl: "https://codecrafty.dev", // your website URL
+  siteUrl: "https://codecrafty.dev",
   generateRobotsTxt: true, // (optional)
-  // ...other options
+  // additional configuration options...
+  additionalPaths: async () =>
+    [
+      "/build",
+      "/how",
+      "/about/contact",
+      "/services/development",
+      "/services/maintenance",
+      "/about/team",
+      "/services/design",
+      "/contact",
+      "/",
+      "/blog",
+      "/about/aboutUs",
+      "/blog/WhyAI",
+      "/services/ai",
+    ].map((url) => ({
+      loc: url, // The URL of the page
+      changefreq: "daily",
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    })),
 };
