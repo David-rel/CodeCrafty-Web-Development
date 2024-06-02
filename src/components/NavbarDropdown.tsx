a12bbda7297ec75ce9963b7ad22acee335ae0808
bakedpotatolord <jeffy9745@gmail.com>
2024-06-01 22:29:20 -0600

    formatting

--- src/components/NavbarDropdown.tsx
+++ src/components/NavbarDropdown.tsx
@@ -2,32 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-
-
 export type Props = {
   path: string;
   name: string;
 }[];
 
 const NavbarDropdown: React.FC<{ props: Props }> = ({ props }) => {
-
-
   const router = useRouter();
-
   const isActive = (pathname: string, paths: Props = [], exact = true) => {
     if (exact) {
       return router.pathname === pathname;
     } else {
       return (
-        router.pathname.startsWith(pathname) || paths.map((path) => path.path).includes(router.pathname)
+        router.pathname.startsWith(pathname) ||
+        paths.map((path) => path.path).includes(router.pathname)
       );
     }
   };
-
-
   return (
     <>
-      {props.map(({path, name}) => (
+      {props.map(({ path, name }) => (
         <Link href={path} legacyBehavior>
           <a
             className={`p-2 ${
